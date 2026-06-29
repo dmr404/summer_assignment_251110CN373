54e96c18113c167327849600e04cd9694fedd650
dmr404 <divyam.rai10may@gmail.com>
2026-06-29 20:48:21 +0530

    Add files via upload

---
+++ Day-12/2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool isArmstrong(int n) {
+    int original = n, sum = 0;
+
+    while(n > 0) {
+        int digit = n % 10;
+        sum += digit * digit * digit;
+        n /= 10;
+    }
+
+    return original == sum;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    if(isArmstrong(n))
+        cout << "Armstrong";
+    else
+        cout << "Not Armstrong";
+
+    return 0;
+}
